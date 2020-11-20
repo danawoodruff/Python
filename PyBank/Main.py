ebb0960d5c9fb43cdfd467ce6f78013e496f30d1
Dana <danasnews1@gmail.com>
2020-11-20 09:12:06 -0600

    Update files

--- PyBank/Main.py
+++ PyBank/Main.py
@@ -1,15 +1,7 @@
 
-#Calculate the changes in "Profit/Losses" over the entire period, then find the average of those changes
-#The greatest increase in profits (date and amount) over the entire period
-#The greatest decrease in losses (date and amount) over the entire period
-#As an example, your analysis should look similar to the one below:
-#Financial Analysis
-#----------------------------
-#Total Months: 86
-#Total: $38382578
-#Average  Change: $-2315.12
-#Greatest Increase in Profits: Feb-2012 ($1926159)
-#Greatest Decrease in Profits: Sep-2013 ($-2196167)
+#Dana Woodruff
+#PyBank
+
 import os
 import csv
 
--- PyPoll/Main.py
+++ PyPoll/Main.py
@@ -1,21 +1,5 @@
-#The total number of votes cast
-#A complete list of candidates who received votes
-#The percentage of votes each candidate won
-#The total number of votes each candidate won
-#The winner of the election based on popular vote.
-#As an example, your analysis should look similar to the one below:
-
-#Election Results
-#-------------------------
-#Total Votes: 3521001
-#-------------------------
-#Khan: 63.000% (2218231)
-#Correy: 20.000% (704200)
-#Li: 14.000% (492940)
-#O'Tooley: 3.000% (105630)
-#-------------------------
-#Winner: Khan
-#-------------------------
+#Dana Woodruff
+#PyPoll Nov 2020
 
 import os
 import csv
